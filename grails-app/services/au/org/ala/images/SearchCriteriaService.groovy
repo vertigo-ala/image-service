7e7893a4916b6a8d997904fd21766a3f16e40b14
Dave Martin <djtfmartin@gmail.com>
2019-07-24 22:01:41 +0100

    caching of search criteriaq definitions

--- grails-app/services/au/org/ala/images/SearchCriteriaService.groovy
+++ grails-app/services/au/org/ala/images/SearchCriteriaService.groovy
@@ -1,35 +1,43 @@
 package au.org.ala.images
 
-import grails.transaction.Transactional
-
-@Transactional
 class SearchCriteriaService {
 
     def logService
 
-    public List<SearchCriteriaDefinition> getCriteriaDefinitionList() {
-        def fields = Image.class.declaredFields
+    List<SearchCriteriaDefinition> criteriaDefinitionList = []
 
-        fields.each { field ->
-            if (field.isAnnotationPresent(SearchableProperty)) {
-                def ann = field.getAnnotation(SearchableProperty)
+    List<SearchCriteriaDefinition> getCriteriaDefinitionList() {
 
-                def criteriaDefinition = SearchCriteriaDefinition.findByTypeAndFieldName(CriteriaType.ImageProperty, field.name)
-                if (!criteriaDefinition) {
-                    logService.debug("Creating new Image Property criteria definition for ${field.name}")
-                    criteriaDefinition = new SearchCriteriaDefinition(name: "Image ${field.name}", type: CriteriaType.ImageProperty, valueType: ann.valueType(), fieldName: field.name, units: ann.units(), description: ann.description())
-                    criteriaDefinition.save(flush: true, failOnError: true)
+        if (!criteriaDefinitionList) {
+
+            try {
+                def fields = Image.class.declaredFields
+
+                fields.each { field ->
+                    if (field.isAnnotationPresent(SearchableProperty)) {
+                        def ann = field.getAnnotation(SearchableProperty)
+
+                        def criteriaDefinition = SearchCriteriaDefinition.findByTypeAndFieldName(CriteriaType.ImageProperty, field.name)
+                        if (!criteriaDefinition) {
+                            logService.debug("Creating new Image Property criteria definition for ${field.name}")
+                            criteriaDefinition = new SearchCriteriaDefinition(name: "Image ${field.name}", type: CriteriaType.ImageProperty, valueType: ann.valueType(), fieldName: field.name, units: ann.units(), description: ann.description())
+                            criteriaDefinition.save(flush: true, failOnError: false)
+                        }
+                    }
                 }
+                criteriaDefinitionList = SearchCriteriaDefinition.list().sort { it.name }
+            } catch (Exception e){
+                log.error(e.getMessage(), e)
             }
         }
 
-        // add a criteria definition for meta data items...
-        def existing = SearchCriteriaDefinition.findByType(CriteriaType.ImageMetadata)
-        if (!existing) {
-            existing = new SearchCriteriaDefinition(name: "Image metadata", type: CriteriaType.ImageMetadata, fieldName: "n/a", valueType: CriteriaValueType.StringDirectEntry, units:"")
-            existing.save(flush: true, failOnError: true)
-        }
+//        // add a criteria definition for meta data items...
+//        def existing = SearchCriteriaDefinition.findByType(CriteriaType.ImageMetadata)
+//        if (!existing) {
+//            existing = new SearchCriteriaDefinition(name: "Image metadata", type: CriteriaType.ImageMetadata, fieldName: "n/a", valueType: CriteriaValueType.StringDirectEntry, units:"")
+//            existing.save(flush: true, failOnError: false)
+//        }
 
-        return SearchCriteriaDefinition.list().sort { it.name }
+        return criteriaDefinitionList
     }
 }
