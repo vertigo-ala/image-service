585b2b74f0e805fa86c1f6dd490dfe2ae37dcdb5
Dave Martin <djtfmartin@gmail.com>
2019-09-09 16:06:48 +0100

    update original_filename to be full URL

--- db-upgrade-0_9_to_1_0.sql
+++ db-upgrade-0_9_to_1_0.sql
@@ -3,6 +3,8 @@ alter table image add recognised_license_id bigint;
 alter table image add occurrence_id character varying(255);
 alter table image add calibrated_by_user character varying(255);
 
+CREATE SEQUENCE IF NOT EXISTS image_metadata_seq AS BIGINT;
+
 CREATE TABLE public.license (
     id bigint NOT NULL,
     version bigint NOT NULL,
@@ -27,7 +29,6 @@ ALTER TABLE ONLY public.license_mapping
 
 update image set mime_type = 'audio/mpeg' where original_filename like '%.mp3';
 
-
 /* Add the occurrence_id to top level table */
 CREATE TEMP TABLE temp_image_occurrence as select image_id, value as occurrence_id from image_meta_data_item imdi where imdi.name='occurrenceId';
 UPDATE image
@@ -35,4 +36,18 @@ SET occurrence_id = subquery.occurrence_id
 FROM (SELECT io.image_id, io.occurrence_id from temp_image_occurrence io) AS subquery
 WHERE image.id = subquery.image_id;
 
-DROP TABLE temp_image_occurrence;
+
+/* Add the occurrence_id to top level table */
+alter table image add full_original_url character varying(255);
+
+CREATE TEMP TABLE temp_image_full_url as select image_id, value as full_original_url from image_meta_data_item imdi where imdi.name='fullOriginalUrl';
+
+UPDATE image
+SET full_original_url = subquery.full_original_url
+FROM (SELECT io.image_id, io.full_original_url from temp_image_full_url io) AS subquery
+WHERE image.id = subquery.image_id;
+
+DROP TABLE temp_image_full_url;
+
+ALTER TABLE image RENAME COLUMN original_filename TO original_filename_old;
+ALTER TABLE image RENAME COLUMN full_original_url TO original_filename;
